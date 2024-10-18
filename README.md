30366a5179c362ab64a82ab2564cee8779f0eeec
Jp4357 <jyotiprakash4357@gmail.com>
2024-10-18 13:55:51 +0530

    Data Ingestion Done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # text-Summarizer
-text-Summarizer
+
+## workflows
+
+1 . Config.yaml
+2 . Params.yaml
+3 . Config entity
+4 . Configuration Manager
+5 . Update the components - Data Ingestion,Data Transformation,Model Trainer
+6 . Create out pipeline -- Training Pipeline , Prediction pipeline
+7 . Frond end -- Api's ,Training Api's ,Batcj prediction Apis
+
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionPipeline
 
-logger.info("hello world")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion_pipeline = DataIngestionPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
